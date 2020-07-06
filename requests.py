c9033b16d00b910c3279e9102bae8503589f39c6
trever <toakumu@gmail.com>
2020-07-06 18:20:34 +0300

    Added function onthe file

--- requests.py
+++ requests.py
@@ -7,3 +7,12 @@ api_key = None
 
 #getting the news base url
 base_url = None
+
+#getting the articlces url
+articles_url = None
+
+def configure_request(app):
+    	global api_key,base_url,articles_url
+	api_key = app.config['NEWS_API_KEY']
+	base_url = app.config['NEWS_SOURCES_BASE_URL']
+	articles_url = app.config['ARTICLES_BASE_URL']
