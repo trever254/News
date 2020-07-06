c5553e6a137691313022c76ded93c49a4297d62a
trever <toakumu@gmail.com>
2020-07-06 18:23:06 +0300

    added another function

--- requests.py
+++ requests.py
@@ -16,3 +16,21 @@ def configure_request(app):
 	api_key = app.config['NEWS_API_KEY']
 	base_url = app.config['NEWS_SOURCES_BASE_URL']
 	articles_url = app.config['ARTICLES_BASE_URL']
+
+def get_sources(category):
+    	'''
+	Function that gets the json response to our url request
+	'''
+	get_sources_url = base_url.format(category,api_key)
+
+	with urllib.request.urlopen(get_sources_url) as url:
+		get_sources_data = url.read()
+		get_sources_response = json.loads(get_sources_data)
+
+		sources_results = None
+
+		if get_sources_response['sources']:
+			sources_results_list = get_sources_response['sources']
+			sources_results = process_sources(sources_results_list)
+
+	return sources_results
