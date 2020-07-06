cd30dce1453816fcd28c17a6b917c2e578a0dbb9
trever <toakumu@gmail.com>
2020-07-06 18:06:33 +0300

    added changes on models file

--- requests.py
+++ requests.py
@@ -1,2 +1,9 @@
 import urllib.request,json
 from .models import Sources,Articles
+from datetime import datetime
+
+#getting the api key
+api_key = None
+
+#getting the news base url
+base_url = None
