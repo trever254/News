ec91c046ce215c7bb75c6d1edb8b8ab3ac68a5df
trever <toakumu@gmail.com>
2020-07-15 12:19:29 +0300

    added changes

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
     #registering the blueprint
-    from .main import  main as main_blueprint
+    from.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     #setting config
